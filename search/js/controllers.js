c69b9d5b16d2174e1c517585fbbb9220fe8cacb4
Nicolas Eeckeloo <neeckeloo@gmail.com>
2013-01-11 12:54:49 +0100

    Add controllers file and improve pager

--- search/js/controllers.js
+++ search/js/controllers.js
@@ -3,46 +3,10 @@
 /* Controllers */
 
 function ItemListCtrl($scope, $http) {
-    var items = [],
-        currentPage = 1,
-        itemCount = null,
-        pageCount = null;
-
     $http.get('items.json').success(function(data) {
-        $scope.initItems(data);
+        $scope.items = data;
+        $scope.pages = new Util.Pager($scope.items);
     });
 
-    $scope.initItems = function(data) {
-        items = data;
-        itemCount = items.length;
-        $scope.updatePageCount();
-    };
-
-    $scope.updateItems = function() {
-        var offset = (currentPage - 1) * $scope.itemCountPerPage;
-
-        $scope.items = [];
-        angular.forEach(items, function(value, key){
-            if (key >= offset && this.length < $scope.itemCountPerPage) {
-                this.push(value);
-            }
-        }, $scope.items);
-    };
-
-    $scope.setPage = function(page) {
-        if (page < pageCount) {
-            currentPage = page;
-        } else {
-            currentPage = pageCount;
-        }
-        $scope.updateItems();
-    };
-
-    $scope.updatePageCount = function() {
-        pageCount =  Math.ceil(itemCount / $scope.itemCountPerPage);
-        $scope.setPage(currentPage);
-    };
-
     $scope.orderProp = 'fiscal_year';
-    $scope.itemCountPerPage = 10;
 }
---
+++ search/js/lib/pager.js
@@ -0,0 +1,96 @@
+(function (util) {
+
+    util.ITEM_COUNT_PER_PAGE = 10;
+
+    util.range = function (start, end) {
+        var rng = [];
+
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+
+        for (var i = start; i < end; i++)
+            rng.push(i);
+
+        return rng;
+    };
+
+    util.Pager = function (data) {
+        var self = this,
+            itemCountPerPage = util.ITEM_COUNT_PER_PAGE;
+
+        self.current = 0;
+
+        self.content = function (index) {
+            if (index > self.pageCount - 1) {
+                index = self.pageCount - 1;
+            } else if (index < 0) {
+                index = 0;
+            }
+
+            if (index != self.current) {
+                self.current = index;
+                return null;
+            }
+            
+            var start = index * self.itemCountPerPage,
+                end = (index * self.itemCountPerPage + self.itemCountPerPage) > data.length
+                    ? data.length
+                    : (index * self.itemCountPerPage + self.itemCountPerPage);
+
+            return data.slice(start, end);
+        };
+
+        self.next = function () {
+            if (!self.canPage('next')) return;
+            self.current++;
+        };
+
+        self.prev = function () {
+            if (!self.canPage('prev')) return;
+            self.current--;
+        };
+
+        self.first = function () {
+            self.current = 0;
+        };
+
+        self.last = function () {
+            self.current = self.pageCount - 1;
+        };
+
+        self.canPage = function (dir) {
+            if (dir === 'next') return self.current < self.pageCount - 1;
+            if (dir === 'prev') return self.current > 0;
+            return false;
+        };
+
+        self.list = function () {
+            var start, end;
+            start = self.current < 5 ? 0 : self.current - 5;
+            end = self.pageCount - self.current < 5
+                ? self.pageCount
+                : self.current + 5;
+            return Util.range(start, end);
+        };
+
+        Object.defineProperty(self, 'itemCountPerPage', {
+            configurable: false,
+            enumerable: false,
+            get: function () { return itemCountPerPage; },
+            set: function (val) {
+                itemCountPerPage = parseInt(val) || itemCountPerPage;
+            }
+        });
+
+        Object.defineProperty(self, 'pageCount', {
+            configurable: false,
+            enumerable: false,
+            get: function () {
+                return Math.ceil(data.length / self.itemCountPerPage);
+            }
+        });
+    };
+
+})(window.Util = window.Util || {});
